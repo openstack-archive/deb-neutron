the2hill/neutron

Tests for extension, db and plugin for LBaaS V2
Adding needed driver interface changes for tests.
Adding LoggingNoop driver needed changes for tests.
Adding extension, plugin, and db unit tests.

Partially-implements: blueprint lbaas-api-and-objmodel-improvement

Change-Id: I1f7762bf3c9484b8bf46ce32ab78574aa081324b
Co-authored-by: Brandon Logan <brandon.logan@rackspace.com>
Co-authored-by: Vijayendra Bhamidipati <vbhamidipati@paypal.com>
Co-authored-by: Craig Tracey <Craig.Tracey@gmail.com>
Co-authored-by: Phillip Toohill <phillip.toohill@rackspace.com>
Co-authored-by: Dustin Lundquist <dustin@null-ptr.net>
Co-authored-by: Doug Wiegley <dougw@a10networks.com>

neutron/services/loadbalancer/drivers/driver_mixins.py
@@ -49,37 +49,29 @@ class BaseRefreshMixin(object):
 class BaseStatsMixin(object):
 
     @abc.abstractmethod
-    def stats(self, context, obj):
+    def stats(self, context, obj_id):
         pass
 
 
 class BaseStatusUpdateMixin(object):
 
     # Status update helpers
-    # Note: You must set self.model_class to an appropriate neutron model
+    # Note: You must set model_class to an appropriate neutron model
     # in your base manager class.
 
     def active(self, context, model_id):
-        if self.model_class is not None:
-            self.driver.plugin.update_status(context, self.model_class,
-                                             model_id, constants.ACTIVE)
+        self.driver.plugin.update_status(context, self.model_class,
+                                         model_id, constants.ACTIVE)
 
     def failed(self, context, model_id):
-        if self.model_class is not None:
-            self.driver.plugin.update_status(context, self.model_class,
-                                             model_id, constants.ERROR)
+        self.driver.plugin.update_status(context, self.model_class,
+                                         model_id, constants.ERROR)
 
 
-class BaseHealthMonitorStatusUpdateMixin(object):
+class BaseDeleteHelperMixin(object):
 
-    def active(self, context, health_monitor_id, pool_id):
-        self.driver.plugin.update_pool_health_monitor(context,
-                                                      health_monitor_id,
-                                                      pool_id,
-                                                      constants.ACTIVE)
+    # DB delete helper
+    # Must define appropriate db delete function
 
-    def failed(self, context, health_monitor_id, pool_id):
-        self.driver.plugin.update_pool_health_monitor(context,
-                                                      health_monitor_id,
-                                                      pool_id,
-                                                      constants.ERROR)
+    def db_delete(self, context, model_id):
+        self.db_delete_method(context, model_id)

openstack-archive/deb-neutron

neutron/services/loadbalancer/drivers/driver_mixins.py
@@ -56,30 +56,26 @@ class BaseStatsMixin(object):
 class BaseStatusUpdateMixin(object):
 
     # Status update helpers
-    # Note: You must set self.model_class to an appropriate neutron model
+    # Note: You must set model_class to an appropriate neutron model
     # in your base manager class.
 
     def active(self, context, model_id):
-        if self.model_class is not None:
-            self.driver.plugin.update_status(context, self.model_class,
-                                             model_id, constants.ACTIVE)
+        self.driver.plugin.db.update_status(context, self.model_class,
+                                            model_id, constants.ACTIVE)
 
     def failed(self, context, model_id):
-        if self.model_class is not None:
-            self.driver.plugin.update_status(context, self.model_class,
-                                             model_id, constants.ERROR)
+        self.driver.plugin.db.update_status(context, self.model_class,
+                                            model_id, constants.ERROR)
+
+    def defer(self, context, model_id):
+        self.driver.plugin.db.update_status(context, self.model_class,
+                                            model_id, constants.DEFERRED)
 
 
-class BaseHealthMonitorStatusUpdateMixin(object):
+class BaseDeleteHelperMixin(object):
 
-    def active(self, context, health_monitor_id, pool_id):
-        self.driver.plugin.update_pool_health_monitor(context,
-                                                      health_monitor_id,
-                                                      pool_id,
-                                                      constants.ACTIVE)
+    # DB delete helper
+    # Must define appropriate db delete function
 
-    def failed(self, context, health_monitor_id, pool_id):
-        self.driver.plugin.update_pool_health_monitor(context,
-                                                      health_monitor_id,
-                                                      pool_id,
-                                                      constants.ERROR)
+    def db_delete(self, context, model_id):
+        self.db_delete_method(context, model_id)